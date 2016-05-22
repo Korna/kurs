dbb3195d6c9d2a96e5f6f93588185310d206867d
Korna <artem-koma@yandex.ru>
2016-05-23 02:05:23 +0600

    sd

--- test/functest.c
+++ test/functest.c
@@ -17,3 +17,18 @@ CTEST(equation_suite, cropofsymbols)
 	ASSERT_EQUAL(a, ch);
 
 }
+
+CTEST(equation_suite, cropofsymbols)
+{
+	//Given
+	const char string[] = "sad";
+	
+	//When
+	char fstring[260];
+	crop(string, fstring);
+	int ch = 'd';
+	int a = fstring[2];
+
+	//Then
+	ASSERT_EQUAL(a, ch);
+
