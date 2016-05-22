6e9eed79e833ae8f6722027c6af4d698ef0907c1
Korna <artem-koma@yandex.ru>
2016-05-22 17:11:51 +0600

    program

---
+++ src/functions.h
@@ -0,0 +1,8 @@
+#ifndef formath
+#define formath
+void exchange (char *ptr[], int j);
+int schr(char str[],char ch);
+int stok(char str[], char *ptr[]);
+void crop(char string[], char *fstring);
+void sort(char *ptr[], int size);
+#endif
---
+++ src/interface.h
@@ -0,0 +1,5 @@
+#ifndef interfaceh
+#define interfaceh
+int interact (char string[]);
+void out (char *ptr[], int size);
+#endif
